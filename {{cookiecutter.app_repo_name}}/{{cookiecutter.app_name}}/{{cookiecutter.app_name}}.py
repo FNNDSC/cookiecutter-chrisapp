98f30d81c0a5fa9051b468af10f270b77ff450e8
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2019-03-29 10:27:04 -0400

    Add some default flags and in-line help templates.

--- {{cookiecutter.app_repo_name}}/{{cookiecutter.app_name}}/{{cookiecutter.app_name}}.py
+++ {{cookiecutter.app_repo_name}}/{{cookiecutter.app_name}}/{{cookiecutter.app_name}}.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python                                            
 #                                                            _
 # {{ cookiecutter.app_name }} {{ cookiecutter.app_type }} app
 #
@@ -13,6 +14,59 @@ import os
 # import the Chris app superclass
 from chrisapp.base import ChrisApp
 
+Gstr_title = """
+
+Generate a title from 
+http://patorjk.com/software/taag/#p=display&f=Doom&t=pluginTitle
+
+"""
+
+Gstr_synopsis = """
+
+(Edit this in-line help for app specifics. At a minimum, the 
+flags below are supported.)
+
+    NAME
+
+       {{ cookiecutter.app_name }}.py 
+
+    SYNOPSIS
+
+        python {{ cookiecutter.app_name }}.py                                         \\
+            [-v <level>] [--verbosity <level>]                          \\
+            [--version]                                                 \\
+            [--man]                                                     \\
+            [--meta]                                                    \\
+            <inputDir>                                                  \\
+            <outputDir> 
+
+    BRIEF EXAMPLE
+
+        * Bare bones execution
+
+            mkdir in out && chmod 777 out
+            python {{ cookiecutter.app_name }}.py   \\
+                                in    out
+
+    DESCRIPTION
+
+        `.{{ cookiecutter.app_name }}py` ...
+
+    ARGS
+
+        [-v <level>] [--verbosity <level>]
+        Verbosity level for app. Not used currently.
+
+        [--version]
+        If specified, print version number. 
+        
+        [--man]
+        If specified, print (this) man page.
+
+        [--meta]
+        If specified, print plugin meta data.
+
+"""
 
 class {{ cookiecutter.app_python_class_name }}(ChrisApp):
     """
@@ -52,15 +106,72 @@ class {{ cookiecutter.app_python_class_name }}(ChrisApp):
     # output directory.
     OUTPUT_META_DICT = {}
  
+    def manPage_show(self):
+        """
+        Print some quick help.
+        """
+        print(Gstr_synopsis)
+
+    def metaData_show(self):
+        """
+        Print the plugin meta data
+        """
+        l_metaData  = dir(self)
+        l_classVar  = [x for x in l_metaData if x.isupper() ]
+        for str_var in l_classVar:
+            str_val = getattr(self, str_var)
+            print("%20s: %s" % (str_var, str_val))
+
     def define_parameters(self):
         """
         Define the CLI arguments accepted by this plugin app.
         """
+        self.add_argument("-v", "--verbosity",
+                            help        = "verbosity level for app",
+                            type        = str,
+                            dest        = 'verbosity',
+                            optional    = True,
+                            default     = "0")
+        self.add_argument('--man',
+                            help        = 'if specified, print man page',
+                            type        = bool,
+                            dest        = 'b_man',
+                            action      = 'store_true',
+                            optional    = True,
+                            default     = False)
+        self.add_argument('--meta',
+                            help        = 'if specified, print plugin meta data',
+                            type        = bool,
+                            dest        = 'b_meta',
+                            action      = 'store_true',
+                            optional    = True,
+                            default     = False)
+        self.add_argument('--version',
+                            help        = 'if specified, print version number',
+                            type        = bool,
+                            dest        = 'b_version',
+                            action      = 'store_true',
+                            optional    = True,
+                            default     = False)
 
     def run(self, options):
         """
         Define the code to be run by this plugin app.
         """
+        if options.b_man:
+            self.manPage_show()
+            sys.exit(0)
+
+        if options.b_meta:
+            self.metaData_show()
+            sys.exit(0)
+
+        if options.b_version:
+            print('Plugin Version: %s' % {{ cookiecutter.app_python_class_name }}.VERSION)
+            sys.exit(0)
+
+        print(Gstr_title)
+        print('Version: %s' % {{ cookiecutter.app_python_class_name }}.VERSION)
 
 # ENTRYPOINT
 if __name__ == "__main__":
