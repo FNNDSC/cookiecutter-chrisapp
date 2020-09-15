758281c6169b3f3992339b122292bd6b8407e421
Jennings Zhang <jenni_zh@protonmail.com>
2020-09-14 20:51:15 -0400

    Simplify setup.py

--- {{cookiecutter.app_repo_name}}/setup.py
+++ {{cookiecutter.app_repo_name}}/setup.py
@@ -1,37 +1,25 @@
-
-import sys
-import os
-
-
-# Make sure we are running python3.5+
-if 10 * sys.version_info[0] + sys.version_info[1] < 35:
-    sys.exit("Sorry, only Python 3.5+ is supported.")
-
-
+from os import path
 from setuptools import setup
 
-
-def readme():
-    print("Current dir = %s" % os.getcwd())
-    print(os.listdir())
-    with open('README.rst') as f:
-        return f.read()
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.rst')) as f:
+    readme = f.read()
 
 setup(
-      name             =   '{{ cookiecutter.app_name }}',
-      # for best practices make this version the same as the VERSION class variable
-      # defined in your ChrisApp-derived Python class
-      version          =   '{{ cookiecutter.app_version }}',
-      description      =   '{{ cookiecutter.app_description }}',
-      long_description =   readme(),
-      author           =   '{{ cookiecutter.author_name }}',
-      author_email     =   '{{ cookiecutter.author_email }}',
-      url              =   '{{ cookiecutter.app_documentation }}',
-      packages         =   ['{{ cookiecutter.app_name }}'],
-      install_requires =   ['chrisapp', 'pudb'],
-      test_suite       =   'nose.collector',
-      tests_require    =   ['nose'],
-      scripts          =   ['{{ cookiecutter.app_name }}/{{ cookiecutter.app_name }}.py'],
-      license          =   'MIT',
-      zip_safe         =   False
-     )
+    name             = '{{ cookiecutter.app_name }}',
+    # for best practices make this version the same as the VERSION class variable
+    # defined in your ChrisApp-derived Python class
+    version          = '{{ cookiecutter.app_version }}',
+    description      = '{{ cookiecutter.app_description }}',
+    long_description = readme,
+    author           = '{{ cookiecutter.author_name }}',
+    author_email     = '{{ cookiecutter.author_email }}',
+    url              = '{{ cookiecutter.app_documentation }}',
+    packages         = ['{{ cookiecutter.app_name }}'],
+    install_requires = ['chrisapp~=1.1.6'],
+    test_suite       = 'nose.collector',
+    tests_require    = ['nose'],
+    scripts          = ['{{ cookiecutter.app_name }}/{{ cookiecutter.app_name }}.py'],
+    license          = 'MIT',
+    zip_safe         = False,
+    python_requires  = '>=3.5',
+)
